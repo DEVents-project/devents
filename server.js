ea94f285ff284fa1e428287c4daaad05bcd4505b
peteco83 <peteco1983@outlook.com>
2020-05-28 14:35:54 +0200

    made file server.js to connect to server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const server = express()
+const createError = require("http-errors")
+const mongoose = require("mongoose")
+const logger = require("morgan")
+
+const port = process.env.PORT || 4000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/devents", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.on("error", (err) => console.log(err))
+mongoose.connection.on("open", () => console.log("database connected"))
+
+server.use(express.json())
+server.use(logger("dev"))
+
+server.use("/", indexRoute)
+
+server.use("/users", usersRoute)
+
+server.use((req, res, next) => {
+    next(createError(404))
+})
+
+server.use((err, req, res, next) => {
+    res.json({ status: err.status, err: err.message })
+})
+
+server.listen(port, () => console.log("server is running"))
