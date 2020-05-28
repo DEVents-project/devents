247037cf0b31b6093594cd79d906699f457c9c63
peteco83 <peteco1983@outlook.com>
2020-05-28 14:49:01 +0200

    Add script nodemon and add the folders for the server

--- server.js
+++ server.js
@@ -6,23 +6,19 @@ const logger = require("morgan")
 
 const port = process.env.PORT || 4000;
 
-mongoose.connect("mongodb://127.0.0.1:27017/devents", { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.connection.on("error", (err) => console.log(err))
-mongoose.connection.on("open", () => console.log("database connected"))
+mongoose.connect("mongodb://127.0.0.1:27017/devents", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on("error", (err) => console.log(err));
+mongoose.connection.on("open", () => console.log("database connected"));
 
-server.use(express.json())
-server.use(logger("dev"))
-
-server.use("/", indexRoute)
-
-server.use("/users", usersRoute)
+server.use(express.json());
+server.use(logger("dev"));
 
 server.use((req, res, next) => {
     next(createError(404))
-})
+});
 
 server.use((err, req, res, next) => {
     res.json({ status: err.status, err: err.message })
-})
+});
 
-server.listen(port, () => console.log("server is running"))
+server.listen(port, () => console.log("server is running"));
