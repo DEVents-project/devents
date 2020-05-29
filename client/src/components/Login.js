6fabdde9c5bc158dde8e1900b2aab3ba11f1c55d
Nadia Daruiz <nadiadaruiz@gmail.com>
2020-05-29 11:23:55 +0200

    add form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/Login.scss';
 
 function Login() {
+    const [email, setEmail] = setEmail(null);
+    const [password, setPassword] = setPassword(null);
+    const [login, setLogin] = setLogin(false)
+
+    function preventRefresh(e) {
+        e.preventDefault()
+    }
+
+    function handleSubmit() {
+
+    }
 
     return (
         <div>
-
+            <form onSubmit={preventRefresh}>
+                <h2>LOG IN</h2>
+                <label> Email
+                    <input type="email" value="email" placeholder="email" required />
+                </label>
+                <label> Password
+                    <input type="password" value="value" placeholder="password" required />
+                </label>
+                <button>LOG IN</button>
+            </form>
         </div>
     )
 }
