706300c61b32ef847afa2fea045bb9d86f021e48
Jaime Segura <jseguraweb@gmail.com>
2020-05-30 15:30:54 +0200

    solve bugs with the nav display

--- client/src/components/NavBarSignedIn.js
+++ client/src/components/NavBarSignedIn.js
@@ -8,6 +8,16 @@ const NavBarSignedIn = () => {
     const [isNavVisible, setIsNavVisible] = useState(false);
     const [isSmallScreen, setIsSmallScreen] = useState(false);
 
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(max-width: 670px)');
+        mediaQuery.addListener(handleMediaQueryChange);
+        handleMediaQueryChange(mediaQuery);
+
+        return () => {
+            mediaQuery.removeListener(handleMediaQueryChange);
+        };
+    }, [])
+
     const handleMediaQueryChange = mediaQuery => {
         if (mediaQuery.matches) {
             setIsSmallScreen(true);
@@ -16,29 +26,23 @@ const NavBarSignedIn = () => {
         }
     };
 
-    useEffect(() => {
-        const mediaQuery = window.matchMedia('(max-width: 670px)');
-        mediaQuery.addListener(handleMediaQueryChange);
-        handleMediaQueryChange(mediaQuery);
-
-        return () => {
-            mediaQuery.removeListener(handleMediaQueryChange);
-        }
-    }, [])
+    const toggleNav = () => {
+        setIsNavVisible(!isNavVisible);
+    };
 
     return (
         <div>
             <header>
                 <Link to="/"><h1 id="logo">DEVents</h1></Link>
-                <FontAwesomeIcon className="menu-bars" icon={faBars} onClick={() => setIsNavVisible(!isNavVisible)} />
-                <nav style={{ display: isNavVisible ? 'flex' : 'none' }}>
+                <FontAwesomeIcon className="menu-bars" icon={faBars} onClick={toggleNav} />
+                <nav style={{ display: isNavVisible || !isSmallScreen ? 'flex' : 'none' }}>
                     <ul className="nav-group">
                         <Link to="/events"><li>Find events</li></Link>
                         <Link to="/register"><li>Add event</li></Link>
                     </ul>
                     <ul className="nav-group">
-                        <Link to="/login"><li>LOGIN</li></Link>
-                        <Link to="/signup"><li>SIGN UP</li></Link>
+                        <Link to="/login"><li>ACCOUNT</li></Link>
+                        <Link to="/signup"><li>SIGN OUT</li></Link>
                     </ul>
                 </nav>
             </header>
