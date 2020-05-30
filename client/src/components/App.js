7c8ac2fdb785eb37c7797f7949dd844573abd275
Jaime Segura <jseguraweb@gmail.com>
2020-05-30 15:53:14 +0200

    add 2nd navbar, when the user is signed out. Put it in the App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,14 +2,21 @@ import React, { useState, Fragment } from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import '../style/App.scss';
 import NavBarSignedIn from "./NavBarSignedIn";
+import NavBarSignedOut from "./NavBarSignedOut";
 
 const App = () => {
   const [test, setTest] = useState(0);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBarSignedIn />
+        {
+          loggedIn ?
+            <NavBarSignedIn />
+            :
+            <NavBarSignedOut />
+        }
         <Switch>
           {/* <Route path="/" exact component={Landing} /> */}
           {/* <Route path="/registration" component={Registration} /> */}
