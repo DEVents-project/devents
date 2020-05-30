a8a620239bf6537748d4b77504234b3d70f70ab3
Dana Shacham <dshacham22@gmail.com>
2020-05-30 13:25:18 +0200

    add schema validator

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -1,9 +1,10 @@
 const Route = require("express").Router();
 const { getEvent, getEvents, postEvent, putEvent, deleteEvent } = require("../controllers/eventController");
+const { validateInputs } = require("../middleware/validator");
 
 Route.get("/", getEvents);
 Route.get("/:id", getEvent);
-Route.post("/", postEvent);
+Route.post("/", validateInputs(), postEvent);
 Route.put("/:id", putEvent);
 Route.delete("/:id", deleteEvent);
 
