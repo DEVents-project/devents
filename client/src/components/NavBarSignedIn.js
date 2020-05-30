48ccf9e2fff0dfcbf1fbeb8de9dd26c42200e538
Jaime Segura <jseguraweb@gmail.com>
2020-05-30 13:51:02 +0200

    add style to component NavbarSignedIn

--- client/src/components/NavBarSignedIn.js
+++ client/src/components/NavBarSignedIn.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style/NavBarSignedIn.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const NavBarSignedIn = () => {
     return (
         <div>
-
+            <header>
+                <Link to="/"><h1 id="logo">DEVents</h1></Link>
+                <FontAwesomeIcon className="menu-bars" icon={faBars} />
+                <nav>
+                    <ul className="nav-group">
+                        <Link to="/events"><li>Find events</li></Link>
+                        <Link to="/register"><li>Add event</li></Link>
+                    </ul>
+                    <ul className="nav-group">
+                        <Link to="/login"><li>LOGIN</li></Link>
+                        <Link to="/signup"><li>SIGN UP</li></Link>
+                    </ul>
+                </nav>
+            </header>
         </div>
     );
 }
