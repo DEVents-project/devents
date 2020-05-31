ccadf971198d3e54b40d53ef893b6c924d6c3237
Jaime Segura <jseguraweb@gmail.com>
2020-05-31 14:38:17 +0200

    style 1st part of the landing site. Also responsive

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import '../style/Landing.scss';
+import Developers from '../assets/img/devs-sitting-2.png';
+import ParticlesBg from 'particles-bg';
 
 const Landing = () => {
     return (
         <div className="landing">
             <main className="main">
-                <h2><span className="underscore">Never miss</span> <br /><span className="keyword">events for developers</span><br /> <span className="underscore">again</span>!</h2>
+                <ParticlesBg color="#8d8d8d" num={50} type="cobweb" bg={true} />
+                <h2><span className="underscore">Never miss</span> <br /><span className="keyword"><strong>events for developers</strong></span><br /> <span className="underscore again">again</span></h2>
                 <div className="container-description">
-                    <h4>Search for conferences
-                    <br />create your own study group
-                    <br />join meetups!</h4>
-                    {/* <h4>Push up your knowledge and social skills!</h4> */}
+                    <h4>Search for conferences</h4>
+                    <h4>Assist to workshops</h4>
+                    <h4>Create your own meetups</h4>
                 </div>
                 <div className="call-buttons">
-                    <button className="action-button">Find event</button>
-                    <button className="action-button">Add event</button>
+                    <button className="button action-button">FIND EVENT</button>
+                    <button className="button action-button">ADD EVENT</button>
                 </div>
+                <img src={Developers} alt="Developers" className="developers" />
             </main>
             <section className="carousel">
                 <p>here comes the carousel</p>
