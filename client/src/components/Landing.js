4b727e850d73ef8fd06a930f4383a796cce9b3ca
Jaime Segura <jseguraweb@gmail.com>
2020-05-31 14:55:31 +0200

    add animation to h4

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../style/Landing.scss';
 import Developers from '../assets/img/devs-sitting-2.png';
 import ParticlesBg from 'particles-bg';
+import Typical from 'react-typical';
+
 
 const Landing = () => {
     return (
@@ -10,9 +12,11 @@ const Landing = () => {
                 <ParticlesBg color="#8d8d8d" num={50} type="cobweb" bg={true} />
                 <h2><span className="underscore">Never miss</span> <br /><span className="keyword"><strong>events for developers</strong></span><br /> <span className="underscore again">again</span></h2>
                 <div className="container-description">
-                    <h4>Search for conferences</h4>
-                    <h4>Assist to workshops</h4>
-                    <h4>Create your own meetups</h4>
+                    <Typical
+                        steps={['Search for conferences', 1500, 'Assist to workshops', 1500, 'Create your own meetups', 1500, 'Push up your social skills', 1500]}
+                        loop={Infinity}
+                        wrapper="h4"
+                    />
                 </div>
                 <div className="call-buttons">
                     <button className="button action-button">FIND EVENT</button>
