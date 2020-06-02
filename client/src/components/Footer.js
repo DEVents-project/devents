e52a0d5f1aaabcf708faa2e9504fd1970df567c6
Nadia Daruiz <nadiadaruiz@gmail.com>
2020-06-02 15:18:07 +0200

    add footer-container class

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 import '../style/Footer.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { faQuestionCircle, faPaperPlane, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
     return (
-        <div className="container">
+        <div className="footer-container">
             <footer className="footer">
-                <FontAwesomeIcon className="icon-help" icon={faQuestionCircle} />
-                <div></div>
+                <FontAwesomeIcon className="icon" icon={faQuestionCircle} />
+                <FontAwesomeIcon className="icon" icon={faPaperPlane} />
+                <FontAwesomeIcon className="icon section" icon={faSearch} />
             </footer>
         </div>
     )
