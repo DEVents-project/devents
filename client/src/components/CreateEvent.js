9b36430cce0f6fb9d13d217e5f7cc0f1ec335e3f
Nadia Daruiz <nadiadaruiz@gmail.com>
2020-06-03 15:52:36 +0200

    finish js file structure

--- client/src/components/CreateEvent.js
+++ client/src/components/CreateEvent.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import '../style/CreateEvent.scss';
 import ParticlesBg from 'particles-bg';
 
 
 const CreateEvent = () => {
-    // time missing - Instead of day, month and year is possible to do Date? 
+    // img missing and hour ? 
     const [name, setName] = useState(null);
     const [hostBy, setHostBy] = useState(null);
     const [city, setCity] = useState(null);
@@ -13,7 +14,7 @@ const CreateEvent = () => {
     const [description, setDescription] = useState(null);
     const [link, setLink] = useState(null);
 
-    const [statusCreated, setStatusCreated] = useState(null)
+    const [statusAdded, setStatusAdded] = useState(null)
 
     const handleCreateEvent = (e) => {
         e.preventDefault()
@@ -27,53 +28,105 @@ const CreateEvent = () => {
             description,
             link
         }
+
+        const eventData = {
+
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(eventInfo)
+
+        }
+        // double que the route - addevent?
+
+        const resp = await fetch("http://localhost:3000/addevent", eventData);
+        const data = await resp.json();
+        console.log("res:", data);
+        if (data.success) {
+            setStatusAdded(true)
+        }
     }
 
 
     return (
         <div className="create-event-container">
-            <form className="event-form">
-
+            <form className="event-form" onSubmit={handleCreateEvent}>
+                {statusAdded ? <Redirect to="/events" /> : null}
                 <h2>CREATE EVENT</h2>
                 <label className="field-event">Title
                     <input
                         type="text"
                         value={name}
-                        name="name" />
+                        id="name"
+                        placeholder="the event title"
+                        required
+                        onChange={(e) => setName(e.target.value)}
+
+                    />
                 </label>
                 <label className="field-event">Host by
                     <input
                         type="text"
                         value={hostBy}
-                        name="hostBy" />
+                        id="hostBy"
+                        placeholder="the host name"
+                        required
+                        onChange={(e) => setHostBy(e.target.value)}
+                    />
+
                 </label>
                 <label className="field-event">Date
                     <input
                         type="date"
                         value={date}
-                        name="date" />
+                        id="date"
+                        placeholder="the event date"
+                        required
+                        onChange={(e) => setDate(e.target.value)}
+                    />
+
                 </label>
-                <label className="field-event">Address
+                <label className="field-event">Location
                     <input
                         type="text"
                         value={address}
-                        name="address" />
+                        id="address"
+                        placeholder="the event location"
+                        required
+                        onChange={(e) => setAddress(e.target.value)}
+                    />
+
                 </label>
                 <label className="field-event">City
                     <input
                         type="text"
                         value={city}
-                        name="city" />
+                        id="city"
+                        placeholder="in which city"
+                        required
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+
                 </label>
                 <label className="field-event">Website
                     <input
                         type="url"
                         value={website}
-                        name="website"
+                        id="website"
+                        placeholder="do you have a website to share"
+                        required
+                        onChange={(e) => setWebsite(e.target.value)}
+
                     />
                 </label>
                 <label className="field-event">Description
-                    <textarea row="10" cols="50" />
+                    <textarea row="10" cols="50"
+                        placehoder="the event details"
+                        id="description"
+                        required
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
                 </label>
                 <button type="submit">PUBLISH EVENT</button>
 
