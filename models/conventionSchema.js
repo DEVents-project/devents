e14fc4ff19629bb509f0041eb492a3a3e5664d16
peteco83 <peteco1983@outlook.com>
2020-06-04 22:28:59 +0200

    Add files to upload conventions and workshops, corrected one thing on eventRoute, add the security file with cors

---
+++ models/conventionSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const conventionSchema = new Schema({
+    title: String,
+    date: String,
+    location: String,
+    city: String,
+    description: String,
+    img: String,
+    url: String,
+})
+
+module.exports = mongoose.model("conventions", conventionSchema)
