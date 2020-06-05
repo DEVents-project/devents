1cab85f0154062488e16f19fa5e19f230fb2810b
Jaime Segura <jseguraweb@gmail.com>
2020-06-05 18:55:59 +0200

    add context and loggedIn variable

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import '../style/App.scss';
 import NavBarSignedIn from "./NavBarSignedIn";
@@ -12,31 +12,35 @@ import Footer from './Footer';
 import Account from "./Account";
 import EventInformation from "./EventInformation";
 import CreateEvent from "./CreateEvent";
+import Context from './Context';
+
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(true);
 
   return (
     <div className="App">
-      <BrowserRouter>
-        {
-          loggedIn ?
-            <NavBarSignedIn />
-            :
-            <NavBarSignedOut />
-        }
-        <Switch>
-          <Route path="/" exact component={Landing} />
-          <Route path="/registration" component={Registration} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/login" component={Login} />
-          <Route path="/events" component={Events} />
-          <Route path="/account" component={Account} />
-          <Route path="/event" component={EventInformation} />
-          <Route path="/addevent" component={CreateEvent} />
-        </Switch>
-      </BrowserRouter>
-      <Footer />
+      <Context.Provider value={{ loggedIn, setLoggedIn }}>
+        <BrowserRouter>
+          {
+            loggedIn ?
+              <NavBarSignedIn />
+              :
+              <NavBarSignedOut />
+          }
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <Route path="/registration" component={Registration} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/login" component={Login} />
+            <Route path="/events" component={Events} />
+            <Route path="/account" component={Account} />
+            <Route path="/event" component={EventInformation} />
+            <Route path="/addevent" component={CreateEvent} />
+          </Switch>
+        </BrowserRouter>
+        <Footer />
+      </Context.Provider>
     </div>
   );
 };
