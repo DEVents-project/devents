5ee499c01bc18ff936297f76a74f18027ea37b92
Dana Shacham <dshacham22@gmail.com>
2020-06-09 13:35:43 +0200

    add github login

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -1,10 +1,9 @@
 const Route = require("express").Router();
 const { getEvent, getEvents, postEvent, putEvent, deleteEvent } = require("../controllers/eventController");
-const { validateInputs } = require("../middleware/validatorEvent");
 
 Route.get("/", getEvents);
 Route.get("/:id", getEvent);
-Route.post("/", validateInputs(), postEvent);
+Route.post("/", postEvent);
 Route.put("/:id", putEvent);
 Route.delete("/:id", deleteEvent);
 
