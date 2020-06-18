c9f1489e54e405d716a85f8ef270ce878aa045b0
Nadia Daruiz <nadiadaruiz@gmail.com>
2020-06-18 16:35:37 +0200

    github is working

--- middleware/githubAuth.js
+++ middleware/githubAuth.js
@@ -1,5 +1,5 @@
 const passport = require("passport");
-const GithubStrategy = require('passport-github2').Strategy;
+const GithubStrategy = require('passport-github').Strategy;
 const dot = require("dotenv").config();
 
 // Github login
@@ -11,17 +11,17 @@ passport.deserializeUser(function (id, cb) {
     cb(null, id);
 });
 
-const clientID = process.env.GITHUB_CLIENT_ID;
-const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+const userID = process.env.GITHUB_CLIENT_ID;
+const userSecret = process.env.GITHUB_CLIENT_SECRET;
 
 passport.use(new GithubStrategy({
-    clientID: clientID,
-    clientSecret: clientSecret,
-    callbackURL: "http://localhost:4000/auth/github/redirect"
+    clientID: userID,
+    clientSecret: userSecret,
+    callbackURL: "/auth/github/redirect"
 },
     function (accessToken, refreshToken, profile, cb) {
-
         console.log(profile);
+
         User.findOrCreate({ githubId: profile.id }, function (err, user) {
             return cb(null, user);
         });
