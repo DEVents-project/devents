dbf890ac30af315d4c2fddf7471fe0e389e004e2
Jaime Segura <jseguraweb@gmail.com>
2020-06-19 08:26:30 +0200

    restore registration module

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,107 +1,78 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { withRouter } from 'react-router-dom'
+
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import ParticlesBg from 'particles-bg';
 import '../style/Registration.scss'
-import  registration from '../assets/img/signup.svg'
-import  createEvent from '../assets/img/createEvent.svg'
-import  publish from '../assets/img/publish.svg'
-import SignUp from './SignUp';
-import Login from "./Login";
+import Signin from '../assets/img/signup.svg'
+import CreateEvent from '../assets/img/createEvent.svg'
+import Publish from '../assets/img/publish.svg';
 
 
 
 
 const Registration = () => {
-  
-    
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
+
+
     return (
         <div className="space-wrap space-navbar">
             <ParticlesBg color="#8d8d8d" num={50} type="cobweb" bg={true} />
-            <h3 className="main-h3">From developers to developers. A place where sharing and finding meetups, workshops and conventions is simple!</h3>
-            
-            <h1 className="question">DO YOU WANT TO SHARE YOUR EVENT? NO PROBLEM!</h1>
-          
+            <div className="main-text">
+                <h1 className="slide-from-left">For developers, by developers.<br /><span className="second-line">Where finding each other is</span>  <span className="underscore-2"> simple !</span></h1>
+            </div>
+
+            <h2 className="question">DO YOU WANT TO SHARE YOUR EVENT? NO PROBLEM!</h2>
+
             <div className="container">
-            
-             {/* square 1 */}
-                    
-             
                 <div className="squares">
                     <div className="img-1">
-                    <img src={registration} alt="registrer" className="img-registration"/> 
+                        <img src={Signin} alt="sing up" className="img-registration" />
                     </div>
-                
-                
-                <div className="aside aside-1"> 
-                       <h2>EASY SIGN UP</h2>
-                       <p>Create an account with only your name and email address.</p>
-                   </div>
-                  
-                   
-                </div>
-                  
-                
-                   
 
-              {/* square 2 */}
-                
+                    <div className="aside aside-1">
+                        <h3 className="title">EASY SIGN UP</h3>
+                        <p className="registration-p">Create an account with only your name and email address.</p>
+                    </div>
+                </div>
+
                 <div className="squares square-2">
-
-                <div className="img-1">
-                <img src={createEvent} alt="create event"  className="img-registration"/>
+                    <div className="img-1">
+                        <img src={CreateEvent} alt="create an event" className="img-registration" />
+                    </div>
+                    <div className="aside aside-2">
+                        <h3 className="title">CREATE AN EVENT</h3>
+                        <p className="registration-p">Share the relevant information about your event.</p>
+                    </div>
                 </div>
-                <div className="aside aside-2"> 
-                       <h2>CREATE AN EVENT</h2>
-                       <p>Share all the relevant information about your event. 
-</p>
-                   
-                   </div>
-                </div>
-                    
 
-                  {/* square 3 */}
-                   
                 <div className="squares square-3 box">
-                   <div className="img-1">
-                   <img src={publish} alt="publish"  className="img-registration"/>
-                   </div>
-                   <div className="aside aside-3"> 
-                       <h2>AND PUBLISH IT!</h2>
-                       <p>One click and that's it. Easy peasy! </p>
-                   </div>
+                    <div className="img-1">
+                        <img src={Publish} alt="publish an event" className="img-registration" />
+                    </div>
+                    <div className="aside aside-3">
+                        <h3 className="title">AND PUBLISH IT!</h3>
+                        <p className="registration-p">One click and that's it. Easy peasy! </p>
+                    </div>
                 </div>
-                   
-                   
-
             </div>
-            
-           
 
-            
-               
-        <div className="buttons-container">
-            <button className="button signup-button">Sign up</button>
-            <button className="button login-button">Login</button>
-        </div>
+            <div className="buttons-container">
+                <Link to='/signup' className="button signup-button">Sign up</Link>
+                <Link to='/login' className="button login-button">Login</Link>
+            </div>
 
-        <BrowserRouter>
-       <Switch>
-           <Route path="/login">
-            <Login/>               
-           </Route>
-       </Switch>
-        </BrowserRouter>
-            
         </div>
     )
 }
 
 export default Registration
 
-                   
-                    
-                   
-                  
-                
-                   
+
+
+
+
+
+
