2b994aa2b3686e0974281a02f75fa3501354b0d3
Dana Shacham <dshacham22@gmail.com>
2020-06-24 11:31:08 +0200

    add buttons

--- client/src/components/EventInformation.js
+++ client/src/components/EventInformation.js
@@ -185,6 +185,20 @@ const EventInformation = (props) => {
                                                             <button className="button link-to-site" onClick={() => { setEditMode(true); localStorage.removeItem('event-info') }}>EDIT</button>
                                                             <button className="button link-to-site delete-button" onClick={(e) => { if (window.confirm('Are you sure you want to delete this event?')) { deleteEvent(e) } }}>DELETE</button>
                                                         </div>
+                                                        <div className="share-buttons">
+                                                            <FacebookShareButton
+                                                                url={window.location.href}
+                                                                quote="Check out this event!"
+                                                                hashtag="#whoscomingwith">
+                                                                <FacebookIcon className="fb-icon" size={40} />
+                                                            </FacebookShareButton>
+                                                            <TwitterShareButton
+                                                                url={window.location.href}
+                                                                quote="Check out this event!"
+                                                                hashtag="#whoscomingwith">
+                                                                <TwitterIcon className="fb-icon" size={40} />
+                                                            </TwitterShareButton>
+                                                        </div>
                                                     </div>
                                                     <div className="event-information-box-two">
                                                         <p className="event-information-description">{eventInfo.description}</p>
